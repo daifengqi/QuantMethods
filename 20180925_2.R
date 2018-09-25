23d033106785820f50662f9f1711e3661b54e94a
internet <internet@uibe.edu>
2018-09-25 20:33:14 +0800

    20180925

--- 20180925_2.R
+++ 20180925_2.R
@@ -7,3 +7,20 @@ ord <- ar(gnp, method = 'mle') # 拟合AR模型
 # 默认aic=TRUE，会使用aic来挑选变量
 # ar(x, aic = TRUE, order.max = NULL,
 #    method = c('yuan-walker', 'burg', 'ols', 'mle') ...)
+
+# ARIMA模型
+dat <- read.table('m-ibm3dx2608.txt', header = T) # 读入文档
+vw <- dat[,3]
+m3 <- arima(vw, order = c(3,0,0))
+
+# 看结果
+m3
+(1-0.1158+0.0187+0.1042)*mean(vw)
+m3$sigma2 # 残差平方和
+ts.plot(m3$residuals)
+
+# 检验残差是否是白噪声
+Box.test(m3$residuals, lag=12, type='Ljung') 
+# 手动调整 自由度=9
+pv <- 1-pchisq(16.352,9) # 卡方分布的p值pchisq()
+pv
