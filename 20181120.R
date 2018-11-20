45c9725af374059f224a879fb02989402b6b0b52
internet <internet@uibe.edu>
2018-11-20 20:37:47 +0800

    20181120

---
+++ 20181120.R
@@ -0,0 +1,25 @@
+setwd('D://data')
+dat <- read.table('taq-cat-t-jan042010.txt', header = T)
+head(dat)
+vol <- dat$size/100
+dat1 <- read.table('taq-cat-cpch-jan042010.txt', header = T)
+cpch <- dat1[,1]
+pch <- dat1[,2]
+cf <- as.factor(cpch)
+length(cf)
+y <- cf[4:37715]
+y1 <- cf[3:37714]
+y2 <- cf[2:37713]
+vol <- vol[2:37716]
+v2 <- vol[2:37713]
+cp1 <- pch[3:37714]
+cp2 <- pch[2:37713]
+cp3 <- pch[1:37712]
+y~ v2+cp1+cp2+cp3+y1+y2
+library(MASS)
+# probit函数
+m1 <- polr(y~ v2+cp1+cp2+cp3+y1+y2, method = 'probit')
+summary(m1)
+names(m1)
+yhat <- m1$fitted.values
+print(yhat[1:5,], digits = 3) # digits:保留三个有效数字
