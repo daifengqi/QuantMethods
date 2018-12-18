0183b4fd5451c5793c5ef6e1e2d8d13b69b731df
internet <internet@uibe.edu>
2018-12-18 20:21:54 +0800

    Update 20181218.R

--- 20181218.R
+++ 20181218.R
@@ -1,6 +1,7 @@
 setwd("D:/QuantMethods/data")
 x <- read.table('m-fac9003.txt', header = T) #前13列是股票，最后一列是市场
 
+### 因子分析(单因子模型)
 # 自变量
 xmtx <- cbind(rep(1,168), x[,14]) # 添加一列截距项
 # 因变量(多元)
@@ -18,6 +19,21 @@ r.square <- 1-(168-2)*D.hat/diag(t(rtn)%*%rtn)
 
 # 查看结果
 t(rbind(beta.hat, sqrt(D.hat), r.square))
-# 第一列：对市场的依赖程度。
-# 第二列：
+# 第一列：对市场的依赖程度；股票的Beta。
+# 第二列：拟合回归的MSE。
 # 第三列：市场因子对股票的解释程度，即拟合好坏。
+
+
+
+### PCA
+rtn <- read.table("m-5clog-9008.txt", header = T) # 5只股票的数据
+pca.cov <- princomp(rtn) # 主成分分析
+names(pca.cov)           # 查看结果包含的内容
+pca.cov$sdev       # 特征根
+summary(pca.cov)   # 结果汇总
+pca.cov$loadings   # 权重(特征向量)
+screeplot(pca.cov) # 碎石图
+# 使用相关系数矩阵做PCA
+pca.corr <- princomp(rtn, cor=T)
+## 其他使用方式同上
+summary(pca.corr)
